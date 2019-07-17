61b291cadced600e59f86fa861b55b1982524988
TheKiller65YT <jackgonser@gmail.con>
2019-07-16 14:25:38 -1000

    v0.01C.2 Added command to yml & set loc feature

--- src/main/java/com/theartemismc/Mafia/Mafia.java
+++ src/main/java/com/theartemismc/Mafia/Mafia.java
@@ -2,6 +2,7 @@ package com.theartemismc.Mafia;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Mafia extends JavaPlugin {
@@ -25,11 +26,34 @@ public class Mafia extends JavaPlugin {
 		if (cmd.getName().equalsIgnoreCase("mafiaadmin")) { // If the player typed /mafiaadmin then do the following, note: If you only registered this executor for one command, you don't need this
 			if (args.length == 0){
 				sender.sendMessage(pretty.prettyChat("XD"));
-			}
-			return true;
-		} //If this has happened the function will return true. 
-	        // If this hasn't happened the value of false will be returned.
-		return false; 
+			} if (args.length == 2 && args[0].equalsIgnoreCase("setLocation")) {
+				if (args[1].equalsIgnoreCase("lobby")) {
+					if (sender instanceof Player) {
+						Player player = (Player) sender;
+						this.getConfig().set("lobby.x", player.getLocation().getBlockX());
+						this.getConfig().set("lobby.y", player.getLocation().getBlockY());
+						this.getConfig().set("lobby.z", player.getLocation().getBlockZ());
+						player.sendMessage(pretty.prettyChat("Set lobby location"));
+						getLogger().info(pretty.prettyConsoleInfo("set lobby location"));
+					} else {
+						sender.sendMessage(pretty.prettyChat("You must be a player to use this command."));
+					}
+				} else if (args[1].equalsIgnoreCase("execution")) {
+					if (sender instanceof Player) {
+						Player player = (Player) sender;
+						this.getConfig().set("execution.x", player.getLocation().getBlockX());
+						this.getConfig().set("execution.y", player.getLocation().getBlockY());
+						this.getConfig().set("execution.z", player.getLocation().getBlockZ());
+						player.sendMessage(pretty.prettyChat("Set execution watching location"));
+						getLogger().info(pretty.prettyConsoleInfo("set execution watching location"));
+					} else {
+						sender.sendMessage(pretty.prettyChat("You must be a player to use this command."));
+					}
+				}
+				return true;
+			} //If this has happened the function will return true. 
+	        // If this hasn't happened the value of false will be returned. 
+		}
+		return false;
 	}
-    
 }
